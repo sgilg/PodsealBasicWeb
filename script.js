ccfad5c578b554b42154a6cab2f2e08d601b6740
Sara <17052812+sgilg@users.noreply.github.com>
2017-08-14 21:28:33 +0200

    loading seal images on button click

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function sayHello(name) {
-    document.getElementById("result").innerHTML = 'Hello ' + name + '!';
+function showSeal() {
+    document.getElementById("sealimage");
+    var images = document.querySelectorAll("#sealimage img");
+    for(var i = 0; i < images.length; i++)
+    {
+      images[i].src = images[i].getAttribute('data-src');
+    }
 }
